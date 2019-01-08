da6ac2d18905cc7e60dbe027b19bbc766909409e
Enkhtuvshin Narmandakh <enhtuwshinn@gmail.com>
2019-01-08 18:46:20 +0800

    Pagination

--- main/client/forms/submission.js
+++ main/client/forms/submission.js
@@ -1,8 +1,6 @@
-/* ----------------------- Form submission mixin ----------------------- */
-
 SubmissionMixin = {
   prototype: {},
-  state: {}
+  state: {},
 };
 
 // formId
@@ -27,13 +25,13 @@ SubmissionMixin.state.isReady = function() {
 /* ----------------------- Form submission detail mixin ----------------------- */
 
 SubmissionDetailMixin = {
-  state: {}
+  state: {},
 };
 
 // submission object
 SubmissionDetailMixin.state.object = function() {
   return FormBuilder.Collections.Submissions.findOne({
-    _id: FlowRouter.getQueryParam('subId')
+    _id: FlowRouter.getQueryParam('subId'),
   });
 };
 
@@ -67,7 +65,7 @@ Template.submissionDelete.events({
   'submit form': function(evt) {
     evt.preventDefault();
     FlowComponents.callAction('onSubmit');
-  }
+  },
 });
 
 /* ----------------------- Form submission detail ----------------------- */
@@ -88,7 +86,82 @@ submissionDetailComponent.state.templateNameToSearch = function() {
 
 /* ----------------------- Form submission list ----------------------- */
 
-var submissionList = Components.define('submissionList', function() {});
+var submissionList = Components.define('submissionList', function() {
+  Session.set('submissionLimit', 10);
+  this._generatePagination = function(page, total, limit, adjacents) {
+    var lastPage, lpm, num, pagination, r1, r2;
+    pagination = [];
+    lastPage = Math.ceil(total / limit);
+    lpm = lastPage - 1;
+    if (lastPage > 0) {
+      if (lastPage < 7 + adjacents * 2) {
+        pagination = (function() {
+          var i, ref, results;
+          results = [];
+          for (
+            num = i = 1, ref = lastPage;
+            1 <= ref ? i <= ref : i >= ref;
+            num = 1 <= ref ? ++i : --i
+          ) {
+            results.push(num);
+          }
+          return results;
+        })();
+      } else {
+        if (page < 1 + adjacents * 3) {
+          r2 = 4 + adjacents * 2;
+          pagination = (function() {
+            var i, ref, results;
+            results = [];
+            for (
+              num = i = 1, ref = r2;
+              1 <= ref ? i <= ref : i >= ref;
+              num = 1 <= ref ? ++i : --i
+            ) {
+              results.push(num);
+            }
+            return results;
+          })();
+          pagination = pagination.concat([-1, lpm, lastPage]);
+        } else if (lastPage - adjacents * 2 > page && page > adjacents * 2) {
+          r1 = page - adjacents;
+          r2 = page + adjacents;
+          pagination = (function() {
+            var i, ref, ref1, results;
+            results = [];
+            for (
+              num = i = ref = r1, ref1 = r2;
+              ref <= ref1 ? i <= ref1 : i >= ref1;
+              num = ref <= ref1 ? ++i : --i
+            ) {
+              results.push(num);
+            }
+            return results;
+          })();
+          pagination = pagination.concat([-1, lpm, lastPage]);
+          pagination = [1, 2, -1].concat(pagination);
+        } else {
+          r1 = lastPage - (1 + adjacents * 3);
+          pagination = (function() {
+            var i, ref, ref1, results;
+            results = [];
+            for (
+              num = i = ref = r1, ref1 = lastPage;
+              ref <= ref1 ? i <= ref1 : i >= ref1;
+              num = ref <= ref1 ? ++i : --i
+            ) {
+              results.push(num);
+            }
+            return results;
+          })();
+          pagination = [1, 2, -1].concat(pagination);
+        }
+      }
+    }
+
+    return pagination;
+  };
+});
 
 submissionList.extend(SubmissionMixin);
 
@@ -97,6 +170,21 @@ submissionList.state.templateNameToSearch = function() {
   return 'SLC' + formObj.code;
 };
 
+submissionList.state.pagination = function() {
+  var limit = Session.get('submission' + 'Limit'),
+    currentPage = Session.get('submission' + 'Offset') || 1,
+    count = Counts.get('submission' + '_counter');
+
+  return {
+    pageable: count > limit,
+    pages: this._generatePagination(currentPage, count, limit, 3),
+    current: currentPage,
+  };
+};
+
+submissionList.state.equal = function(a, b) {
+  return a === b;
+};
 // main list
 submissionList.state.objects = function() {
   FlowRouter.watchPathChange();
@@ -111,4 +199,40 @@ submissionList.state.objects = function() {
 };
 Template.registerHelper('formatDate', function(date) {
   return moment(date).format('YYYY-MM-DD');
+});
+
+Template.submissionList.onCreated(function() {
+  this.autorun(function() {
+    var offset;
+    if (!Session.get('submission' + 'Offset')) {
+      offset = 1;
+    } else {
+      offset = Session.get('submission' + 'Offset');
+    }
+    var skip = Session.get('submission' + 'Limit') * (offset - 1);
+    Meteor.subscribe(
+      'submissionList',
+      FlowRouter.getParam('formId'),
+      Session.get('submission' + 'Limit'),
+      skip
+    );
+  });
+});
+
+Template.submissionList.events({
+  'click #pagination li a': function(event) {
+    event.preventDefault();
+
+    if (!$(event.target).hasClass('active')) {
+      var page = parseInt($(event.target).text());
+
+      if (page) {
+        Session.set('submission' + 'Offset', page);
+        $('#pagination li.active').removeClass('active');
+        return $(event.target)
+          .parent()
+          .addClass('active');
+      }
+    }
+  },
 });
